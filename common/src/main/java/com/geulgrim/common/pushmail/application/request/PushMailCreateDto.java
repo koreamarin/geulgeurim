b67529595969f7458d128d05788cf21729a31d2d
이세은 <senyvee9@gmail.com>
2024-04-25 12:05:45 +0900

    feat: 푸시메일 dto 생성

---
+++ common/src/main/java/com/geulgrim/common/pushmail/application/request/PushMailCreateDto.java
@@ -0,0 +1,37 @@
+package com.geulgrim.common.pushmail.application.request;
+
+import com.geulgrim.common.pushmail.domain.PushMail;
+import com.geulgrim.common.pushmail.domain.PushMailDomain;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PushMailCreateDto {
+
+    private String receiverAddress;
+    
+    private Long senderId;
+
+    private Long[] favoriteJobs; //관심 공고 리스트
+
+    private String domain;
+
+    public PushMail toEntity(PushMailCreateDto dto) {
+
+        return PushMail.builder()
+                .receiverAddress(dto.getReceiverAddress())
+                .senderId(dto.getSenderId())
+                .favoriteJobs(dto.getFavoriteJobs())
+                .domain(PushMailDomain.valueOf(dto.getDomain()))
+                .title(PushMailDomain.valueOf(domain).generateTitle())
+                .content(PushMailDomain.valueOf(domain).generateContent())
+                .isChecked(false)
+                .build();
+    }
+
+}
---
+++ common/src/main/java/com/geulgrim/common/pushmail/application/response/PushMailResponseDto.java
@@ -0,0 +1,26 @@
+package com.geulgrim.common.pushmail.application.response;
+
+import com.geulgrim.common.pushmail.domain.PushMailDomain;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PushMailResponseDto {
+
+    private String receiverAddress;
+
+    private Long senderId;
+
+    private Long[] favoriteJobs;
+
+    private PushMailDomain domain;
+
+    private String title;
+
+    private String content;
+}
