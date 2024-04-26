652c2d00b475f0b3f2a75dcbc458939ae4cf6e8f
류지원 <awldnjs2@naver.com>
2024-04-26 12:01:39 +0900

    Fix: Securiy 기본상태로 변경, SecurityConfig.java 변경내역 삭제

--- auth/src/main/java/com/geulgrim/auth/security/config/SecurityConfig.java
+++ auth/src/main/java/com/geulgrim/auth/security/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,22 +12,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic(config -> config.disable())
-                .csrf(config -> config.disable())
-                .cors(config -> config.disable())
-                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated()
-                )
-
-        ;
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
