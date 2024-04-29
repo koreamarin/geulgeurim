2cb060f9d0f678c6a0ae742150c57ff38f74a11f
koreamarin <awldnjs2@gmail.com>
2024-04-30 03:47:34 +0900

    Feat: 설정 파일 수정

--- auth/src/main/java/com/geulgrim/auth/user/application/service/OAuth2UserService.java
+++ auth/src/main/java/com/geulgrim/auth/user/application/service/OAuth2UserService.java
@@ -17,18 +17,6 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     @Override       // loadUser가 실행될 시점엔 이미 AccessToken이 발급되어 있다
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);        // AccessToken으로 User정보를 조회하는 메서드
-//        Map<String, Object> properties = oAuth2User.getAttributes();
-//        Map<String, Object> kakao_account = (Map<String, Object>) properties.get("kakao_account");
-//
-//        String id = oAuth2User.getAttributes().get("id").toString();
-//        String connected_at = oAuth2User.getAttributes().get("connected_at").toString();
-//        String thumbnail_image_url = (String) ((Map<String, Object>) kakao_account.get("profile")).get("thumbnail_image_url");
-//        String nickname = (String) ((Map<String, Object>) kakao_account.get("profile")).get("nickname");
-//        String name = (String) kakao_account.get("name");
-//        String email = (String) kakao_account.get("email");
-//        String phone_number = (String) kakao_account.get("phone_number");
-//        String birthyear = (String) kakao_account.get("birthyear");
-//        String birthday = (String) kakao_account.get("birthday");
 
         // Role generate
         List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
@@ -40,6 +28,29 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
                 .getUserNameAttributeName();
 
         // DB 저장로직이 필요하면 추가
+        Map<String, Object> properties = oAuth2User.getAttributes();
+        Map<String, Object> kakao_account = (Map<String, Object>) properties.get("kakao_account");
+
+        String id = oAuth2User.getAttributes().get("id").toString();
+        String connected_at = oAuth2User.getAttributes().get("connected_at").toString();
+        String thumbnail_image_url = (String) ((Map<String, Object>) kakao_account.get("profile")).get("thumbnail_image_url");
+        String nickname = (String) ((Map<String, Object>) kakao_account.get("profile")).get("nickname");
+        String name = (String) kakao_account.get("name");
+        String email = (String) kakao_account.get("email");
+        String phone_number = (String) kakao_account.get("phone_number");
+        String birthyear = (String) kakao_account.get("birthyear");
+        String birthday = (String) kakao_account.get("birthday");
+
+        System.out.println(id);
+        System.out.println(connected_at);
+        System.out.println(thumbnail_image_url);
+        System.out.println(nickname);
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(phone_number);
+        System.out.println(birthyear);
+        System.out.println(birthday);
+
         return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), userNameAttributeName);
     }
 }
