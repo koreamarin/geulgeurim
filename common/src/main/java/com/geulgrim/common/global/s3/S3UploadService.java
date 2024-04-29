f244c521093fe8642e2a4dab077cbd76e128c86f
Juhyun Lee <jennysophia3@naver.com>
2024-04-29 14:04:40 +0900

    Remove: 안 쓰는 패키지 삭제

--- common/src/main/java/com/geulgrim/common/global/s3/S3UploadService.java
+++ common/src/main/java/com/geulgrim/common/global/s3/S3UploadService.java
@@ -1,19 +1,13 @@
 package com.geulgrim.common.global.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
--- common/src/main/java/com/geulgrim/common/global/s3/config/S3Config.java
+++ common/src/main/java/com/geulgrim/common/global/s3/config/S3Config.java
@@ -1,9 +1,7 @@
 package com.geulgrim.common.global.s3.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
