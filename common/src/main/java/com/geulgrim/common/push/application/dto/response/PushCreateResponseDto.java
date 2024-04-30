112942c071b0fe9aa17eda7481ad0175deb990c1
이세은 <senyvee9@gmail.com>
2024-04-30 11:05:33 +0900

    Fix: repsonse 정상동작 위한 PushCreateResponseDto getter 추가

--- common/src/main/java/com/geulgrim/common/push/application/dto/response/PushCreateResponseDto.java
+++ common/src/main/java/com/geulgrim/common/push/application/dto/response/PushCreateResponseDto.java
@@ -4,10 +4,16 @@ import com.geulgrim.common.push.domain.PushDomain;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class PushCreateResponseDto {
 
