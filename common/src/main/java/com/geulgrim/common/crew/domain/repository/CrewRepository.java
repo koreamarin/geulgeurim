2464d2d8be8a25abbfbac46d62fb104ee88ebeb6
Juhyun Lee <jennysophia3@naver.com>
2024-05-02 11:39:17 +0900

    Remove: common에서 crew 삭제

--- common/src/main/java/com/geulgrim/common/crew/domain/repository/CrewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.geulgrim.common.crew.domain.repository;
-
-import com.geulgrim.common.crew.domain.entity.Crew;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrewRepository extends JpaRepository<Crew, Long> {
-
-}
