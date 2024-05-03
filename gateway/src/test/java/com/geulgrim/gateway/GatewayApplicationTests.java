f1810b25638640e2da4c9087541c6790bb3c6ec3
김무준 <kmoojun@gmail.com>
2024-05-03 21:21:59 +0900

    fix: 의존성 수정

--- gateway/src/test/java/com/geulgrim/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/geulgrim/gateway/GatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.geulgrim.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatewayApplicationTests.class)
 class GatewayApplicationTests {
 
 	@Test
