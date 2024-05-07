edba390242c1b084483fb03415ca2eb044cd1388
Juhyun Lee <jennysophia3@naver.com>
2024-05-07 14:32:57 +0900

    feat: 크로 모집 신청 페이지 구현

--- vite-ts/src/sections/crew/view/crew-list-view.tsx
+++ vite-ts/src/sections/crew/view/crew-list-view.tsx
@@ -1,13 +1,100 @@
-import Box from "@mui/material/Box"
-import Container from "@mui/material/Container"
+import orderBy from 'lodash/orderBy';
+import { useState, useCallback } from 'react';
 
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import { paths } from 'src/routes/paths';
+
+import { useDebounce } from 'src/hooks/use-debounce';
+
+import { POST_SORT_OPTIONS } from 'src/_mock';
+import { useGetPosts, useSearchPosts } from 'src/api/blog';
+
+import { useSettingsContext } from 'src/components/settings';
+
+import { IPostItem } from 'src/types/blog';
+
+// import PostList from 'src/sections/blog/post-list';
+import PostSort from 'src/sections/blog/post-sort';
+import PostSearch from 'src/sections/blog/post-search';
+import CrewList from '../crew-list';
 
 export default function CrewListView() {
+  const settings = useSettingsContext();
+
+  const [sortBy, setSortBy] = useState('latest');
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const debouncedQuery = useDebounce(searchQuery);
+
+  const { posts, postsLoading } = useGetPosts();
+
+  const { searchResults, searchLoading } = useSearchPosts(debouncedQuery);
+
+  const dataFiltered = applyFilter({
+    inputData: posts,
+    sortBy,
+  });
+
+  const handleSortBy = useCallback((newValue: string) => {
+    setSortBy(newValue);
+  }, []);
+
+  const handleSearch = useCallback((inputValue: string) => {
+    setSearchQuery(inputValue);
+  }, []);
   return (
-    <Container>
-      <Box>
-        크루모집 메인페이지
-      </Box>
+    <Container maxWidth={settings.themeStretch ? false : 'lg'}>
+      <Typography
+        variant="h3"
+        sx={{
+          my: { xs: 3, md: 5 },
+        }}
+      >
+        크루모집
+      </Typography>
+
+      <Stack
+        spacing={3}
+        justifyContent="space-between"
+        alignItems={{ xs: 'flex-end', sm: 'center' }}
+        direction={{ xs: 'column', sm: 'row' }}
+        sx={{ mb: { xs: 3, md: 5 } }}
+      >
+        <PostSearch
+          query={debouncedQuery}
+          results={searchResults}
+          onSearch={handleSearch}
+          loading={searchLoading}
+          hrefItem={(title: string) => paths.post.details(title)}
+        />
+
+        <PostSort sort={sortBy} onSort={handleSortBy} sortOptions={POST_SORT_OPTIONS} />
+      </Stack>
+
+      {/* 게시글들 */}
+      <CrewList posts={dataFiltered} loading={postsLoading} />
+      {/* <PostList posts={dataFiltered} loading={postsLoading} /> */}
     </Container>
-  )
+  );
 }
+
+// -------------------------
+const applyFilter = ({ inputData, sortBy }: { inputData: IPostItem[]; sortBy: string }) => {
+  if (sortBy === 'latest') {
+    return orderBy(inputData, ['createdAt'], ['desc']);
+  }
+
+  if (sortBy === 'oldest') {
+    return orderBy(inputData, ['createdAt'], ['asc']);
+  }
+
+  if (sortBy === 'popular') {
+    return orderBy(inputData, ['totalViews'], ['desc']);
+  }
+
+  return inputData;
+};
