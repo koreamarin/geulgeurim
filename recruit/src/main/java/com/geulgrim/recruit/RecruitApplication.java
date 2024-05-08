086fabea045e9e662c47c5dc10a80357f7bf32ae
김무준 <power649@naver.com>
2024-05-08 11:16:31 +0900

    fix: application.java 수정

--- recruit/src/main/java/com/geulgrim/recruit/RecruitApplication.java
+++ recruit/src/main/java/com/geulgrim/recruit/RecruitApplication.java
@@ -2,8 +2,10 @@ package com.geulgrim.recruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RecruitApplication {
 
     public static void main(String[] args) {
