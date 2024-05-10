823c04fc999404a040e8ddba9acb88f580edec81
윤지현 <goon03077@gmail.com>
2024-05-10 17:58:44 +0900

    Feat: 작품 생성, 조회 구현중

--- common/src/main/java/com/geulgrim/common/piece/domain/repository/PieceRepository.java
+++ common/src/main/java/com/geulgrim/common/piece/domain/repository/PieceRepository.java
@@ -3,5 +3,15 @@ package com.geulgrim.common.piece.domain.repository;
 import com.geulgrim.common.piece.domain.entity.Piece;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface PieceRepository extends JpaRepository<Piece, Long> {
+
+    // 회원의 소유 작품 전체 조회
+    ArrayList<Piece> findAllByOwnerId(Long ownerId);
+
+    // 작품 상세조회
+    Optional<Piece> findByPieceId(Long pieceId);
 }
--- common/src/main/java/com/geulgrim/common/piece/presentation/PieceController.java
+++ common/src/main/java/com/geulgrim/common/piece/presentation/PieceController.java
@@ -6,10 +6,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/common/piece")
@@ -23,4 +22,22 @@ public class PieceController {
     public ResponseEntity<PieceResponseDto> findById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(pieceService.findById(id), HttpStatus.OK);
     }
+
+    @PostMapping("/create")
+    @Operation(summary = "작품등록", description = "작품 정보를 입력받아 작품을 등록하는 api입니다.")
+    public ResponseEntity<PieceCreateRequestDto> create(@RequestBody PieceCreateRequestDto dto){
+        return new ResponseEntity<>(pieceService.create(dto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = "작품검색", description = "userId, 검색조건, 검색어, 정렬조건을 입력받아 조건에 해당하는 작품들을 반환하는 api입니다.")
+    public ResponseEntity<List<PieceResponseDto>> findAll(
+            @RequestParam(name = "user_id", required = true) Long userId,
+            @RequestParam(name = "condition", defaultValue = "title") String condition,
+            @RequestParam(name = "key_word", defaultValue = "") String key_word,
+            @RequestParam(name = "sort", defaultValue = "created_at") String sort
+            ){
+        return new ResponseEntity<>(pieceService.findAllPiece(), HttpStatus.OK);
+    }
+
 }
