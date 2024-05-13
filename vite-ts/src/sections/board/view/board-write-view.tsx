0ee0c2d7c481105a92d1c8d2f11663255179d081
김무준 <power649@naver.com>
2024-05-13 17:59:28 +0900

    feat: 게시물 조회 작성

--- vite-ts/src/sections/board/view/board-write-view.tsx
+++ vite-ts/src/sections/board/view/board-write-view.tsx
@@ -24,22 +24,7 @@ export default function BoardWriteView({ id }: Props) {
   const preview = useBoolean();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [positions, setPositions] = useState({
-    선화: 0, // 선화
-    채색: 0, // 채색
-    배경: 0, // 배경
-    PD: 0, // PD
-    스토리: 0, // 스토리
-    콘티: 0, // 콘티
-  });
   const [files, setFiles] = useState<(File | string)[]>([]);
-
-  const handleChange =
-    (prop: keyof typeof positions) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = event.target.value;
-      const numValue = value === '' ? 0 : parseInt(value, 10);
-      setPositions({ ...positions, [prop]: numValue });
-    };
 
   const navigate = useNavigate();
 
@@ -47,7 +32,6 @@ export default function BoardWriteView({ id }: Props) {
     console.log('Title:', title);
     console.log('Description:', description);
     console.log('Files:', files);
-    console.log('Positions:', positions);
     navigate('/community/crew');
   };
 
@@ -79,82 +63,77 @@ export default function BoardWriteView({ id }: Props) {
   };
 
   return (
-    <Container>
-      <Typography variant="h3">크루 모집하기 {id}</Typography>
-      <TextField
-        sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', gap: 2 }}
-        label="제목"
-        variant="outlined"
-        fullWidth
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <Box sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', gap: 2 }}>
-        {/* Position Fields in a single row */}
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 1 }}>
-          {Object.keys(positions).map((key) => (
-            <TextField
-              key={key}
-              type="number"
-              label={key}
-              variant="outlined"
-              sx={{ width: '16%' }} // Adjusts width to fit all fields in a single row
-              value={positions[key as keyof typeof positions]}
-              onChange={handleChange(key as keyof typeof positions)}
-              inputProps={{ min: 0 }} // Prevents negative numbers
-            />
-          ))}
-        </Box>
+    <Container sx={{ p: 3 }}>
+      <Box
+        sx={{ borderBottom: '3px solid black', marginLeft: 15, marginRight: 15, marginBottom: 3 }}
+      >
+        <Typography variant="h3" component="div" sx={{ color: 'black', ml: 3 }}>
+          자유 게시판
+        </Typography>
+      </Box>
 
+      <Box sx={{ marginLeft: 15, marginRight: 15, marginBottom: 3 }}>
         <TextField
-          label="설명"
+          sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', gap: 2 }}
+          label="제목"
           variant="outlined"
           fullWidth
-          multiline
-          rows={4}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <Stack spacing={5}>
-          <Card>
-            <CardHeader
-              title="이미지 업로드"
-              action={
-                <FormControlLabel
-                  control={<Switch checked={preview.value} onClick={preview.onToggle} />}
-                  label="Show Thumbnail"
-                />
-              }
-            />
-            <CardContent>
-              <Upload
-                multiple
-                thumbnail={preview.value}
-                files={files}
-                onDrop={handleDropMultiFile}
-                onRemove={handleRemoveFile}
-                onRemoveAll={handleRemoveAllFiles}
-                onUpload={() => console.info('ON UPLOAD')}
-              />
-            </CardContent>
-          </Card>
-        </Stack>
+        <Box sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', gap: 2 }}>
+          {/* Position Fields in a single row */}
 
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            gap: 1,
-            marginTop: 2,
-            marginBottom: 4,
-          }}
-        >
-          <Button variant="contained" onClick={handleSubmit}>
-            등록
-          </Button>
-          <Button variant="outlined" onClick={handleCancel}>
-            취소
-          </Button>
+          <TextField
+            label="내용"
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={8}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <Stack spacing={5}>
+            <Card>
+              <CardHeader
+                title="이미지 업로드"
+                action={
+                  <FormControlLabel
+                    control={<Switch checked={preview.value} onClick={preview.onToggle} />}
+                    label="Show Thumbnail"
+                  />
+                }
+              />
+              <CardContent>
+                <Upload
+                  multiple
+                  thumbnail={preview.value}
+                  files={files}
+                  onDrop={handleDropMultiFile}
+                  onRemove={handleRemoveFile}
+                  onRemoveAll={handleRemoveAllFiles}
+                  onUpload={() => console.info('ON UPLOAD')}
+                />
+              </CardContent>
+            </Card>
+          </Stack>
+
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              gap: 1,
+              marginTop: 2,
+              marginBottom: 4,
+            }}
+          >
+            <Button variant="contained" onClick={handleSubmit}>
+              등록
+            </Button>
+            <Button variant="outlined" onClick={handleCancel}>
+              취소
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Container>
