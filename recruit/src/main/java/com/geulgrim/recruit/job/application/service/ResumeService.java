64447dd89ee8397c987ac489736103ce236606e1
류지원 <awldnjs2@naver.com>
2024-05-19 23:59:52 +0900

    Fix: 지원한 이력서 API 조회 시 파일URL 나오도록 수정

--- recruit/src/main/java/com/geulgrim/recruit/job/application/service/ResumeService.java
+++ recruit/src/main/java/com/geulgrim/recruit/job/application/service/ResumeService.java
@@ -375,6 +375,7 @@ public class ResumeService {
             GetSubmittedResumeResponse getSubmittedResumeResponse = GetSubmittedResumeResponse.builder()
                     .resumeId(submittedResume.getResume().getResumeId())
                     .resultStatus(String.valueOf(submittedResume.getResultStatus()))
+                    .resumeUrl(submittedResume.getResumeUrl())
                     .build();
             getSubmittedResumeResponses.add(getSubmittedResumeResponse);
         }
